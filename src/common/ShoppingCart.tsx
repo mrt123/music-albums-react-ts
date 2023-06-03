51376aed54c6487c421de767b74b66540a0522cb
Tomasz Sz <tomasz.szawara@gmail.com>
2023-06-03 14:18:51 +0200

    mapped shopping cart items

--- src/common/ShoppingCart.tsx
+++ src/common/ShoppingCart.tsx
@@ -1,13 +1,20 @@
+import { useAppSelector } from "..";
+
 const ShoppingCart = () => {
   // Na podstawie jakiej czesci stanu reduxa mielibysmy stworzyc liste itemów?
 
+  const shoppingCartItems = useAppSelector((state) => state.shoppingCart.items);
+
+  const itemsElements = shoppingCartItems.map((item, index) => {
+    return (
+      <div key={index}>
+        {item.label} {item.price}
+      </div>
+    );
+  });
+
   // Jak zrobic zeby JSX byl budowany bardziej dynamicznie?
-  return (
-    <div>
-      <div>Title1 10.99</div>
-      <div>Title2 9.99</div>
-    </div>
-  );
+  return <div>{itemsElements}</div>;
 };
 
 export default ShoppingCart;
